b53c2b2c0b1ed6fb8ebd2b8d14285266787537f2
Eman <aziz.eman@hotmail.com>
2024-02-02 18:33:44 -0500

    database connects to either test db or cloud db depending on env variable

--- utils/db.js
+++ utils/db.js
@@ -1,11 +1,20 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize(`${process.env.DB_EXTERNAL_URL}?ssl=true`);
+const db =
+  process.env.NODE_ENV === 'test'
+    ? new Sequelize('cctestdb', 'postgres', 'mypassword', {
+        dialect: 'postgres',
+      })
+    : new Sequelize(`${process.env.DB_EXTERNAL_URL}?ssl=true`);
 
 (async () => {
   try {
     db.authenticate();
-    console.log('we have connected to our Postgres DB');
+    console.log(
+      process.env.NODE_ENV === 'test'
+        ? 'we have connect to our local test DB'
+        : 'we have connected to our Cloud DB'
+    );
   } catch (error) {
     console.log(error);
   }
