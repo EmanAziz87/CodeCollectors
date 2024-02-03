973da04660ec1da6e9026bbce4dd10792a2bc407
Eman <aziz.eman@hotmail.com>
2024-02-03 15:16:54 -0500

    added hubs api endpoints to get all hubs and to update hubs subscriber count.

--- controllers/hubs.js
+++ controllers/hubs.js
@@ -1,8 +1,26 @@
 const hubsRouter = require('express').Router();
 const Hubs = require('../models/Hubs');
 
-hubsRouter.get('/', (req, res) => {
-  res.send('this is the hubs route');
+hubsRouter.get('/', async (req, res, next) => {
+  try {
+    const allHubs = await Hubs.findAll();
+    res.send(allHubs);
+  } catch (error) {
+    next(error);
+  }
+});
+
+hubsRouter.patch('/:id', async (req, res, next) => {
+  const id = req.params.id;
+  const hub = await Hubs.findByPk(id);
+  hub.subscribers = hub.subscribers + 1;
+  await hub.save();
+
+  try {
+    res.send(hub);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = hubsRouter;
