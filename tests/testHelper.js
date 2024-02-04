7f7a77bd4c2189e4df58cafc074abec02a833fe1
Eman <aziz.eman@hotmail.com>
2024-02-04 14:11:20 -0500

    implemented integration tests for posts api. created middle ware for extracting user info from request token on appropriate api calls.

--- tests/testHelper.js
+++ tests/testHelper.js
@@ -1,8 +1,36 @@
 const Users = require('../models/Users');
+const Posts = require('../models/Posts');
+
+const posts = [
+  {
+    title: 'JavaScript Recursive trick',
+    author: 'tim',
+    content: 'nothing to see',
+  },
+  {
+    title: 'another great trick',
+    author: 'tim',
+    content: 'tricked you',
+  },
+  {
+    title: 'prototypal inheritance',
+    author: 'tim',
+    content: 'here is how pi works...',
+  },
+];
+
+const getAllPosts = async () => {
+  const posts = await Posts.findAll();
+  return posts.map((post) => post.toJSON());
+};
+
+const getAPost = async (id) => {
+  return await Posts.findByPk(id);
+};
 
 const getAllUsers = async () => {
   const users = await Users.findAll();
   return users.map((user) => user.toJSON());
 };
 
-module.exports = { getAllUsers };
+module.exports = { getAllUsers, getAllPosts, getAPost, posts };
