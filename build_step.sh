7191963a5ace86083f8ec07701ee1a555cd0a47b
Eman <aziz.eman@hotmail.com>
2024-02-21 10:19:25 -0500

    updated script

--- build_step.sh
+++ build_step.sh
@@ -4,6 +4,6 @@ echo "Build script"
 
 npm install
 
-cd ./client && npm install --include=dev && npm run build
+cd ./client && npm install --production=false && npm run build
 
 cd ../ && npm run start
